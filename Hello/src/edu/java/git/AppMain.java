8dd85965cc5d503dacf9eaf883a9f13c16c10d5c
Bangsooyong <pkmzxcv777@naver.com>
2016-11-11 14:49:26 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -3,7 +3,7 @@ package edu.java.git;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO. GITHUB01~");
+		System.out.println("HELLO. Git~!!");
 		System.out.println("HELLO. GITHUB02~");
         
         System.out.println("HELLO. GITHUB03~");
