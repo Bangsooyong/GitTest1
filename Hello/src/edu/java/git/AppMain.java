89ae04eabf6ad4b03bb8bf577bcddf1da1159588
Bangsooyong <pkmzxcv777@naver.com>
2016-11-11 14:51:40 +0900

    홍길동 고침

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO. Git~!!");
-		System.out.println("HELLO. GITHUB02~");
+		System.out.println("HELLO. GitHub!!~");
         
         System.out.println("HELLO. GITHUB03~");
 	}
