21166cd2c9dc6aed5ea2e1a3c13e2d36f4c4775f
user <user@user-pc>
2016-11-11 14:11:02 +0900

    두번째 커밋

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -3,8 +3,8 @@ package edu.java.git;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO. GITHUB~");
-
+		System.out.println("HELLO. GITHUB01~");
+		System.out.println("HELLO. GITHUB02~");
 	}
 
 }
